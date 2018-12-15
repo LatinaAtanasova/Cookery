cd50a429b2701df193c6cd6c625251b533cf8624
Latina <latina.sasheva@gmail.com>
2018-12-15 21:22:12 +0200

    Adding .gitignore file and mapping table

---
+++ Cookery.Models/RecipeProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cookery.Models
+{
+    public class RecipeProduct
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
